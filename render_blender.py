8e433f2c051f97abfc8d7f7421a89aafd6b9d991
Konstantinos Tertikas <ktertikas@gmail.com>
2021-12-01 13:15:50 +0000

    Modification of scripts to render only images, and addition of a dockerfile that builds a proper docker image

--- render_blender.py
+++ render_blender.py
@@ -1,15 +1,13 @@
-# A simple script that uses blender to render views of a single object by rotation the camera around it.
-# Also produces depth map at the same time.
+# A simple script that uses blender to render views of a single object by rotating the camera around it.
 #
-# Tested with Blender 2.9
+# Tested with Blender 2.9.2
 #
 # Example:
 # blender --background --python mytest.py -- --views 10 /path/to/my.obj
 #
 
-import argparse, sys, os, math, re
+import argparse, sys, os, math
 import bpy
-from glob import glob
 
 parser = argparse.ArgumentParser(description='Renders given obj file by rotation a camera around it.')
 parser.add_argument('--views', type=int, default=30,
@@ -24,14 +22,12 @@ parser.add_argument('--remove_doubles', type=bool, default=True,
                     help='Remove double vertices to improve mesh quality.')
 parser.add_argument('--edge_split', type=bool, default=True,
                     help='Adds edge split filter.')
-parser.add_argument('--depth_scale', type=float, default=1.4,
-                    help='Scaling that is applied to depth. Depends on size of mesh. Try out various values until you get a good result. Ignored if format is OPEN_EXR.')
-parser.add_argument('--color_depth', type=str, default='8',
-                    help='Number of bit per channel used for output. Either 8 or 16.')
 parser.add_argument('--format', type=str, default='PNG',
                     help='Format of files generated. Either PNG or OPEN_EXR')
-parser.add_argument('--resolution', type=int, default=600,
+parser.add_argument('--resolution', type=int, default=256,
                     help='Resolution of the images.')
+parser.add_argument('--device', type=str, default='cpu',
+                    help='Device to be used for rendering. Can be either cpu or cuda.')
 parser.add_argument('--engine', type=str, default='BLENDER_EEVEE',
                     help='Blender internal engine for rendering. E.g. CYCLES, BLENDER_EEVEE, ...')
 
@@ -45,7 +41,6 @@ render = bpy.context.scene.render
 
 render.engine = args.engine
 render.image_settings.color_mode = 'RGBA' # ('RGB', 'RGBA', ...)
-render.image_settings.color_depth = args.color_depth # ('8', '16')
 render.image_settings.file_format = args.format # ('PNG', 'OPEN_EXR', 'JPEG, ...)
 render.resolution_x = args.resolution
 render.resolution_y = args.resolution
@@ -57,6 +52,18 @@ scene.view_layers["View Layer"].use_pass_normal = True
 scene.view_layers["View Layer"].use_pass_diffuse_color = True
 scene.view_layers["View Layer"].use_pass_object_index = True
 
+# device preferences
+preferences = context.preferences
+cycles_preferences = preferences.addons["cycles"].preferences
+cuda_devices, _ = cycles_preferences.get_devices()
+if args.device == "cuda":
+    for i, dev in enumerate(cuda_devices):
+        if "NVIDIA" in dev.name:
+            print("Enabling rendering on device ", dev.name)
+            cuda_devices[i].use = True
+            cycles_preferences.compute_device_type = "CUDA"
+            context.scene.cycles.device = "GPU"
+
 nodes = bpy.context.scene.node_tree.nodes
 links = bpy.context.scene.node_tree.links
 
@@ -66,84 +73,6 @@ for n in nodes:
 
 # Create input render layer node
 render_layers = nodes.new('CompositorNodeRLayers')
-
-# Create depth output nodes
-depth_file_output = nodes.new(type="CompositorNodeOutputFile")
-depth_file_output.label = 'Depth Output'
-depth_file_output.base_path = ''
-depth_file_output.file_slots[0].use_node_format = True
-depth_file_output.format.file_format = args.format
-depth_file_output.format.color_depth = args.color_depth
-if args.format == 'OPEN_EXR':
-    links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
-else:
-    depth_file_output.format.color_mode = "BW"
-
-    # Remap as other types can not represent the full range of depth.
-    map = nodes.new(type="CompositorNodeMapValue")
-    # Size is chosen kind of arbitrarily, try out until you're satisfied with resulting depth map.
-    map.offset = [-0.7]
-    map.size = [args.depth_scale]
-    map.use_min = True
-    map.min = [0]
-
-    links.new(render_layers.outputs['Depth'], map.inputs[0])
-    links.new(map.outputs[0], depth_file_output.inputs[0])
-
-# Create normal output nodes
-scale_node = nodes.new(type="CompositorNodeMixRGB")
-scale_node.blend_type = 'MULTIPLY'
-# scale_node.use_alpha = True
-scale_node.inputs[2].default_value = (0.5, 0.5, 0.5, 1)
-links.new(render_layers.outputs['Normal'], scale_node.inputs[1])
-
-bias_node = nodes.new(type="CompositorNodeMixRGB")
-bias_node.blend_type = 'ADD'
-# bias_node.use_alpha = True
-bias_node.inputs[2].default_value = (0.5, 0.5, 0.5, 0)
-links.new(scale_node.outputs[0], bias_node.inputs[1])
-
-normal_file_output = nodes.new(type="CompositorNodeOutputFile")
-normal_file_output.label = 'Normal Output'
-normal_file_output.base_path = ''
-normal_file_output.file_slots[0].use_node_format = True
-normal_file_output.format.file_format = args.format
-links.new(bias_node.outputs[0], normal_file_output.inputs[0])
-
-# Create albedo output nodes
-alpha_albedo = nodes.new(type="CompositorNodeSetAlpha")
-links.new(render_layers.outputs['DiffCol'], alpha_albedo.inputs['Image'])
-links.new(render_layers.outputs['Alpha'], alpha_albedo.inputs['Alpha'])
-
-albedo_file_output = nodes.new(type="CompositorNodeOutputFile")
-albedo_file_output.label = 'Albedo Output'
-albedo_file_output.base_path = ''
-albedo_file_output.file_slots[0].use_node_format = True
-albedo_file_output.format.file_format = args.format
-albedo_file_output.format.color_mode = 'RGBA'
-albedo_file_output.format.color_depth = args.color_depth
-links.new(alpha_albedo.outputs['Image'], albedo_file_output.inputs[0])
-
-# Create id map output nodes
-id_file_output = nodes.new(type="CompositorNodeOutputFile")
-id_file_output.label = 'ID Output'
-id_file_output.base_path = ''
-id_file_output.file_slots[0].use_node_format = True
-id_file_output.format.file_format = args.format
-id_file_output.format.color_depth = args.color_depth
-
-if args.format == 'OPEN_EXR':
-    links.new(render_layers.outputs['IndexOB'], id_file_output.inputs[0])
-else:
-    id_file_output.format.color_mode = 'BW'
-
-    divide_node = nodes.new(type='CompositorNodeMath')
-    divide_node.operation = 'DIVIDE'
-    divide_node.use_clamp = False
-    divide_node.inputs[1].default_value = 2**int(args.color_depth)
-
-    links.new(render_layers.outputs['IndexOB'], divide_node.inputs[0])
-    links.new(divide_node.outputs[0], id_file_output.inputs[0])
 
 # Delete default cube
 context.active_object.select_set(True)
@@ -215,19 +144,15 @@ cam_constraint.target = cam_empty
 stepsize = 360.0 / args.views
 rotation_mode = 'XYZ'
 
-model_identifier = os.path.split(os.path.split(args.obj)[0])[1]
-fp = os.path.join(os.path.abspath(args.output_folder), model_identifier, model_identifier)
+model_id = args.obj.split("/")[-3]
+fp = os.path.join(os.path.abspath(args.output_folder), model_id)
 
 for i in range(0, args.views):
     print("Rotation {}, {}".format((stepsize * i), math.radians(stepsize * i)))
 
-    render_file_path = fp + '_r_{0:03d}'.format(int(i * stepsize))
+    render_file_path = os.path.join(fp, '%06d'%i)
 
     scene.render.filepath = render_file_path
-    depth_file_output.file_slots[0].path = render_file_path + "_depth"
-    normal_file_output.file_slots[0].path = render_file_path + "_normal"
-    albedo_file_output.file_slots[0].path = render_file_path + "_albedo"
-    id_file_output.file_slots[0].path = render_file_path + "_id"
 
     bpy.ops.render.render(write_still=True)  # render still
 
