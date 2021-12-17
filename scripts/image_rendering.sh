1cbcd5d9deb6d5e8157e705874a170b7defe8c3c
Konstantinos Tertikas <ktertikas@gmail.com>
2021-12-17 18:13:41 +0200

    Not sure what changed here

--- scripts/image_rendering.sh
+++ scripts/image_rendering.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in /datasets/ShapeNetCore.v2/*/; 
-do 
-    x=`basename $i`; 
+for i in /datasets/ShapeNetCore.v2/*/;
+do
+    x=`basename $i`;
     find $i -name *.obj -print0 | xargs -0 -n1 -P12 -I {} blender --background --python render_blender.py -- --output_folder /datasets/tmp/$x/ --views 12 --device=cuda --resolution 256 --engine CYCLES {};
 done
